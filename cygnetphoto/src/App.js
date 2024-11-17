f584c21f6779066f4533cdc05e4c3d2c6a13ee04
Dalton <dlocke6@gmail.com>
2024-11-17 10:43:31 -0500

    New dev

--- cygnetphoto/src/App.js
+++ cygnetphoto/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Navbar from "./componets/navbar";
 
 function App() {
   return (
     <div>
-    
+      <Navbar/>
     </div>
   );
 }
---
+++ cygnetphoto/src/componets/navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <div className="text-black">
+            <h1>Cygnet Photo</h1>
+        </div>
+        );
+}
+
+export default Navbar
