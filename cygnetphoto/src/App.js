9244860b722fbc084e3b75fbe91f6f08398f13cc
Dalton <dlocke6@gmail.com>
2024-12-07 02:08:37 -0500

    Updating webpages

--- cygnetphoto/src/App.js
+++ cygnetphoto/src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Navbar from "./componets/navbar";
+import Hero from "./componets/hero";
+import Footer from "./componets/footer";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
+      <Footer />
     </div>
+
   );
 }
+
 
 export default App;
